50de3e969774dfed3bc2121847d59d75fdaafa3b
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-04-14 13:19:55 -0400

    Finaliza a configuração do nativewind com typescript

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,9 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
+    <View>
+      <Text>Hello World!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "black",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 24,
-    color: "white",
-  },
-});
